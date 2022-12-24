e78dab3438bb92c13664fdd06fb1e401f2b9435e
pablo-suarez <jpsuarez2014@gmail.com>
2022-12-23 23:55:01 -0500

    Configurado GraphQL y Docker

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Development
+
+1. Clone the project
+2. Copy ```env.template``` and rename ```.env```
+3. Run
+```
+npm install
+```
+4. Set up the image (Docker desktop)
+```
+docker-compose up -d
+```
+5. Run backend Nest
+```
+npm run start
+```
+6. Check site
+```
+localhost:4000
+```
---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault
+      ],
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
