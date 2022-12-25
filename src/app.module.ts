07d25148983ecbc7d1c62b1b09bbc385eea81a36
pablo-suarez <jpsuarez2014@gmail.com>
2022-12-25 15:18:19 -0500

    Fixed DB MySQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
@@ -14,6 +17,16 @@ import { UsersModule } from './users/users.module';
       plugins: [
         ApolloServerPluginLandingPageLocalDefault
       ],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+      autoLoadEntities: true,
     }),
     UsersModule,
   ],
